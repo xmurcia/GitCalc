181081e4bf043b0b4088e97a5eff0598564f2e7c
Daniel Perez <dperez@elpuig.xeill.net>
2016-03-02 17:43:31 +0100

    Commit buttons separated

---
+++ src/calc/Buttons.java
@@ -0,0 +1,34 @@
+package calc;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Buttons extends JFrame {
+
+	protected JButton jbnButtons[];
+	protected JPanel jplMaster;
+	protected JPanel jplBackSpace;
+	protected JPanel jplControl;
+
+	public Buttons() throws HeadlessException {
+		super();
+	}
+
+	public Buttons(GraphicsConfiguration arg0) {
+		super(arg0);
+	}
+
+	public Buttons(String arg0) throws HeadlessException {
+		super(arg0);
+	}
+
+	public Buttons(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+	}
+
+}
